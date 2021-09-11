07c234939543df2ede44f410f4fe7ef3dbf6a2c1
Maulana Sodiqin <maulana@pmg.id>
2021-09-12 00:39:40 +0700

    Maulana Sodiqin | Improvement Banner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,13 @@ import Banner from '../components/Banner/index.vue'
     <Navbar />
     <Banner>
         <h1 class="text-[100px] text-white font-bold">Wellcome to PSUDevteam</h1>
-        <button>Pelajari Lebih Lanjut</button>
+        <div class="flex space-x-[20px]">
+            <button
+                class="rounded-lg bg-yellow-300 border-4 border-yellow-100 shadow-md hover:bg-opacity-0 text-white hover:text-yellow-400 hover:border-yellow-300 font-medium w-[200px] h-[60px]"
+            >Bergabung Sekarang</button>
+            <button
+                class="rounded-lg border-4 border-yellow-300 text-yellow-400 shadow-md hover:bg-yellow-300 hover:border-yellow-100 hover:text-white font-medium w-[200px] h-[60px]"
+            >Pelajari Lebih Lanjut</button>
+        </div>
     </Banner>
 </template>
